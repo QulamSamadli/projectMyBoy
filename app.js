48e955e0c8a51914588c71084577db06f6fd2c09
QulamSamadli <qulam999@gmail.com>
2023-09-18 16:53:13 +0400

    MyBoy projects finished

--- app.js
+++ app.js
@@ -1,19 +1,39 @@
-const initSlider =()=>{
+const initSlider = () => {
+  const slideButtons = document.querySelectorAll(
+    ".slider-wrapper .slide-button"
+  );
+  const sliderScrollbar = document.querySelector(
+    ".container .scrollbar"
+  );
+  const scrollbarThumb = sliderScrollbar.querySelector(
+    ".scrollbar-pursue2"
+  );
+  const imgList = document.querySelector(".slider-wrapper .img-list");
+  const maxScrollLeft = imgList.scrollWidth - imgList.clientWidth;
 
-const slideButtons = document.querySelectorAll(".slider-wrapper .slide-button")
+  slideButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const direction = button.id === `prev-slide` ? -1 : 1;
+      const scrollamount = imgList.clientWidth * direction;
+      imgList.scrollBy({ left: scrollamount, behavior: "smooth" });
+    });
+  });
 
-slideButtons.forEach((button)=>{
+  const handleSlideButtons = () => {
+    slideButtons[0].style.display = imgList.scrollLeft <= 0 ? "none" : "block";
+    slideButtons[1].style.display =
+      imgList.scrollLeft >= maxScrollLeft ? "none" : "block";
+  };
+  const updateScrollThumbPosition=()=>{
+    const scrollPosition = imgList.scrollLeft
+    const thumbPosition = (scrollPosition/maxScrollLeft)*(sliderScrollbar.clientWidth-scrollbarThumb.offsetWidth);
+    scrollbarThumb.style.left=`${thumbPosition}px`
+  }
 
-button.addEventListener("click",()=>{
+  imgList.addEventListener("scroll", () => {
+    handleSlideButtons();
+    updateScrollThumbPosition();
+  });
+};
 
-    console.log(button);
-})
-
-
-})
-
-
-}
-
-
-window.addEventListener("load",initSlider)
+window.addEventListener("load", initSlider);
